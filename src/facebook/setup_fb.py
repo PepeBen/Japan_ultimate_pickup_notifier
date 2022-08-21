4c78689b205499fea92ca9dcbf7b95e3d4ac5bd2
PepeBen <81073289+PepeBen@users.noreply.github.com>
2022-08-21 20:27:43 +0900

    Add basic files and folder stucture. Update README file with version 0.01 changes

---
+++ src/facebook/setup_fb.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.5
+# Copyright (C) 2022 Ben Pepe
+import os
+
+# This script will do foo
+
+
+class foo(object):
+    def __init__(self):
+
+    def bar(self):
+
+
+    def baz(self):
